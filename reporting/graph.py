10a7a422dc56a58b1ddf35b1cb04fa346d927f62
artie-sh <artie.sh.87@gmail.com>
2016-02-03 23:20:06 +0100

    graph tutorial started

--- reporting/graph.py
+++ reporting/graph.py
@@ -1,56 +1,37 @@
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import patches, path, animation
+import datetime
+import matplotlib.pyplot as plt
+import random
+
+#date1 = datetime.date(1995, 1, 1)
+#date2 = datetime.date(2004, 4, 12)
+xs = list()
+ys = list()
+
+for i in range(0, 1000, 1):
+    x = 2*random.random()
+    y = 2*random.random()
+    if x <= 1:
+        if y <= 1:
+            continue
+        else:
+            while y > 1:
+                y = 2*random.random()
+    else:
+        if y >= 1:
+            continue
+        else:
+            while y < 1:
+                y = 2*random.random()
+
+    xs.append(x)
+    ys.append(y)
 
 
-fig, ax = plt.subplots()
+x = np.array(xs)
+y = np.array(ys)
+plt.axis([0, 2, 0, 2])
 
-data = np.random.randn(1000)
-n, bins = np.histogram(data, 100)
-
-# get the corners of the rectangles for the histogram
-left = np.array(bins[:-1])
-right = np.array(bins[1:])
-bottom = np.zeros(len(left))
-top = bottom + n
-nrects = len(left)
-
-# here comes the tricky part -- we have to set up the vertex and path
-# codes arrays using moveto, lineto and closepoly
-
-# for each rect: 1 for the MOVETO, 3 for the LINETO, 1 for the
-# CLOSEPOLY; the vert for the closepoly is ignored but we still need
-# it to keep the codes aligned with the vertices
-nverts = nrects*(1 + 3 + 1)
-verts = np.zeros((nverts, 2))
-codes = np.ones(nverts, int) * path.Path.LINETO
-codes[0::5] = path.Path.MOVETO
-codes[4::5] = path.Path.CLOSEPOLY
-verts[0::5, 0] = left
-verts[0::5, 1] = bottom
-verts[1::5, 0] = left
-verts[1::5, 1] = top
-verts[2::5, 0] = right
-verts[2::5, 1] = top
-verts[3::5, 0] = right
-verts[3::5, 1] = bottom
-
-barpath = path.Path(verts, codes)
-patch = patches.PathPatch(barpath, facecolor='green', edgecolor='yellow', alpha=0.5)
-ax.add_patch(patch)
-
-ax.set_xlim(left[0], right[-1])
-ax.set_ylim(bottom.min(), top.max())
-
-
-def animate(i):
-    # simulate new data coming in
-    data = np.random.randn(1000)
-    n, bins = np.histogram(data, 100)
-    top = bottom + n
-    verts[1::5, 1] = top
-    verts[2::5, 1] = top
-    return [patch, ]
-
-ani = animation.FuncAnimation(fig, animate, 100, repeat=False, blit=True)
+plt.plot(xs, ys, 'ro')
+plt.ylabel('amount of cases passed')
 plt.show()
